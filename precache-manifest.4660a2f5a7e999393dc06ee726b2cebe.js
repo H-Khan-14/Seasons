6f01a1740da9ada5a7f640d9566831bfa5433df8
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-09 17:22:20 -0400

    Updates

---
+++ precache-manifest.4660a2f5a7e999393dc06ee726b2cebe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d437c792baba5f999744",
+    "url": "/Seasons/static/js/runtime~main.d437c792.js"
+  },
+  {
+    "revision": "5b2a6c2a7662e498055c",
+    "url": "/Seasons/static/js/main.5b2a6c2a.chunk.js"
+  },
+  {
+    "revision": "3aa4f7a755e9c927d4b6",
+    "url": "/Seasons/static/js/1.3aa4f7a7.chunk.js"
+  },
+  {
+    "revision": "5b2a6c2a7662e498055c",
+    "url": "/Seasons/static/css/main.03a9229e.chunk.css"
+  },
+  {
+    "revision": "2b41a6164cc580f370850e2f6e7b183c",
+    "url": "/Seasons/index.html"
+  }
+];
